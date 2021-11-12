39afa11f01c3f043109523bfe4a9aea706e601e2
Hoki Joyner <henjjoyn@ut.utm.edu>
2021-11-11 18:48:16 -0600

    Fourth Commit: new main.cpp asking user for number n, then sums that number from 1 to n and prints out the value.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 using namespace std;
 
+int sum(int factor);
+
 int main(){
-  cout << "Hello!";
+  int user_number = 0;
+  cout << "Hello!\n";
+  cout << "Please enter a number: ";
+  cin >> user_number;
+  cin.get();
+  int user_number_sum = 0;
+  user_number_sum = sum(user_number);
+  cout << user_number << "'s Sum from 1 to " << user_number << "\n: "<< user_number_sum << endl;
   return 0;
+}
+
+int sum(int factor){
+  int sum = 0;
+  for(int i = 0; i<=factor; i++){
+    sum += i;
+  }
+  return sum;
 }
