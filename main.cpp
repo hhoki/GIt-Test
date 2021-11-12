840c4bb4600f49065c6f52fd191aba8d54a46996
Hoki Joyner <henjjoyn@ut.utm.edu>
2021-11-11 19:03:38 -0600

    Sixth Commit: new main with product function, printing out product of user's number from 1 to n.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int sum(int factor);
-
+int product(int factor);
 int main(){
   int user_number = 0;
   cout << "Hello!\n";
@@ -12,13 +12,23 @@ int main(){
   int user_number_sum = 0;
   user_number_sum = sum(user_number);
   cout << user_number << "'s Sum from 1 to " << user_number << "\n: "<< user_number_sum << endl;
+  int user_number_product = 0;
+  user_number_product = product(user_number);
+  cout << user_number << "'s Product from 1 to " << user_number << "\n: " << user_number_product << endl;
   return 0;
 }
 
 int sum(int factor){
   int sum = 0;
-  for(int i = 0; i<=factor; i++){
+  for(int i = 1; i<=factor; i++){
     sum += i;
   }
   return sum;
+}
+int product(int factor){
+  int product = 1;
+  for(int i = 1; i<=factor; i++){
+    product *= i;
+  }
+  return product;
 }
